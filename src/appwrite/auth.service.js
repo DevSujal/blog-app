1597e39927386129132860602037864836992e65
DevSujal <nimjesujal2004@gmail.com>
2024-06-12 09:24:00 +0530

    Added auth service of appwrite which includes createAccount login getCurrectUser and logout

---
+++ src/appwrite/auth.service.js
@@ -0,0 +1,59 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (!userAccount) {
+        return userAccount;
+      }
+
+      return this.login({ email, password });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+
+  async getCurrectUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("appwrite getuser error : ", error);
+    }
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("issue in logout : ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
