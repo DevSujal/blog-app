07fd1c769479b3e64fbbc3c3e46705904e04ea72
DevSujal <nimjesujal2004@gmail.com>
2024-06-13 19:37:14 +0530

    add login and signup functionality using input and button components used react hook form for better implementation add protected component for the protection of the route

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,10 @@ import Header from "./Header/Header";
 import LogoutButton from "./Header/LogoutButton";
 import Container from "./Container/Container";
 import Logo from "./Logo";
+import Button from "./Button"
+import Input from "./Input"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import SelectBtn from "./SelectBtn";
 
-export {Footer, Header, LogoutButton, Container, Logo}
+export {Footer, Header, LogoutButton, Container, Logo, Button, Input, Login, PostCard, SelectBtn}
